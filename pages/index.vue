5430ba6f1effc75a33cdfaa8ddff7a2e80b39092
Culttm <culttm@gmail.com>
2018-08-22 16:50:28 +0300

    smth

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,15 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-travis-digital
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+    <div class="title">
+      Hallo! Mother fuckers!
     </div>
+    <nuxt-link :to="{name: 'two'}">To the two!</nuxt-link>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
+  export default {
   components: {
-    AppLogo
   }
 }
 </script>
