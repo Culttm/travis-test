690d2fa7cfa8aed71b4143c14500302bb3a17bd6
Culttm <culttm@gmail.com>
2018-08-22 19:21:39 +0300

    smth

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,13 +3,6 @@
     <nuxt/>
   </div>
 </template>
-
-<script>
-  export default {
-
-  }
-</script>
-
 <style>
   html {
     font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
@@ -27,33 +20,8 @@
     margin: 0;
   }
 
-  .button--green {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #3b8070;
-    color: #3b8070;
-    text-decoration: none;
-    padding: 10px 30px;
-  }
-
-  .button--green:hover {
-    color: #fff;
-    background-color: #3b8070;
-  }
-
-  .button--grey {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #35495e;
-    color: #35495e;
-    text-decoration: none;
-    padding: 10px 30px;
-    margin-left: 15px;
-  }
-
-  .button--grey:hover {
-    color: #fff;
-    background-color: #35495e;
+  .container {
+    text-align: center;
   }
 </style>
 
